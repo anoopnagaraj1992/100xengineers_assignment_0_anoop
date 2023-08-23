c327364fd235715b1bf695fbaa269456a95c3e8d
anoopnagaraj1992 <92.anoop@gmail.com>
2023-08-23 17:20:24 -0400

    Simple Chat App code

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('send message', (data) => {
+        // Broadcast to all clients including the sender
+        io.emit('receive message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
