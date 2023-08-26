be59ef776aa104123333acf8c9c6050fe41572e9
anoopnagaraj1992 <92.anoop@gmail.com>
2023-08-26 00:22:45 -0400

    added word replaced with emoji code

--- server.js
+++ server.js
@@ -8,10 +8,27 @@ const io = socketIo(server);
 
 app.use(express.static(__dirname));
 
+const emojiMap = {
+    'hey': '👋',
+    'lol': '😂',
+    'like': '👍',
+    'congratulations': '🎉',
+    'react': '⚛️' // Using the atom symbol to represent React.js
+};
+
+function replaceWithEmojis(message) {
+    for (const [text, emoji] of Object.entries(emojiMap)) {
+        const regex = new RegExp(`\\b${text}\\b`, 'gi'); // Matches the word using word boundaries and ignoring case
+        message = message.replace(regex, emoji);
+    }
+    return message;
+}
+
 io.on('connection', (socket) => {
     console.log('a user connected');
 
     socket.on('send message', (data) => {
+        data.message = replaceWithEmojis(data.message); // Replace text with emojis
         // Broadcast to all clients including the sender
         io.emit('receive message', data);
     });
